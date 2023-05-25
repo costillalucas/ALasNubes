4992468bd14423a69c59ae9975aee3fbb666de8b
yasmin97mdp <yasminemilio97@gmail.com>
2023-05-25 02:40:14 +0200

    climabuscador.html armado, borre el clima del aside

--- js/custom.js
+++ js/custom.js
@@ -1,84 +1,79 @@
-const result = document.querySelector('.result');
-const form = document.querySelector('.get-weather');
-const nameCity = Salta;
-const nameCountry = Argentina;
+const result = document.querySelector(".result");
+const form = document.querySelector(".get-weather");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    if (nameCity.value === '' || nameCountry.value === '') {
-        showError('Ambos campos son obligatorios...');
-        return;
-    }
+  const nameCity = document.getElementById("city").value;
+  const nameCountry = document.getElementById("country").value;
 
-    callAPI(nameCity.value, nameCountry.value);
-    //console.log(nameCity.value);
-    //console.log(nameCountry.value);
-})
+  if (nameCity === "" || nameCountry === "") {
+    showError("Ambos campos son obligatorios...");
+    return;
+  }
 
-function callAPI(city, country){
-    const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+  callAPI(nameCity, nameCountry);
+});
 
-    fetch(url)
-        .then(data => {
-            return data.json();
-        })
-        .then(dataJSON => {
-            if (dataJSON.cod === '404') {
-                showError('Ciudad no encontrada...');
-            } else {
-                clearHTML();
-                showWeather(dataJSON);
-            }
-            //console.log(dataJSON);
-        })
-        .catch(error => {
-            console.log(error);
-        })
+function callAPI(city, country) {
+  const apiId = "9011bbea4b1d31e153788ef764891ac0";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+  fetch(url)
+    .then((data) => {
+      return data.json();
+    })
+    .then((dataJSON) => {
+      if (dataJSON.cod === "404") {
+        showError("Ciudad no encontrada...");
+      } else {
+        clearHTML();
+        showWeather(dataJSON);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-function showWeather(data){
-    const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data;
+function showWeather(data) {
+  const {
+    name,
+    main: { temp, temp_min, temp_max },
+    weather: [arr],
+  } = data;
 
-    const degrees = kelvinToCentigrade(temp);
-    const min = kelvinToCentigrade(temp_min);
-    const max = kelvinToCentigrade(temp_max);
+  const degrees = kelvinToCentigrade(temp);
+  const min = kelvinToCentigrade(temp_min);
+  const max = kelvinToCentigrade(temp_max);
 
-    const content = document.createElement('div');
-    content.innerHTML = `
-        <h5>Clima en ${name}</h5>
-        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
-        <h2>${degrees}°C</h2>
-        <p>Max: ${max}°C</p>
-        <p>Min: ${min}°C</p>
-    `;
+  const content = document.createElement("div");
+  content.innerHTML = `
+    <h5>Clima en ${name}</h5>
+    <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+    <h2>${degrees}°C</h2>
+    <p>Max: ${max}°C</p>
+    <p>Min: ${min}°C</p>
+  `;
 
-    result.appendChild(content);
-
-    /* console.log(name);
-    console.log(temp);
-    console.log(temp_max);
-    console.log(temp_min);
-    console.log(arr.icon); */
+  result.appendChild(content);
 }
 
-function showError(message){
-    //console.log(message);
-    const alert = document.createElement('p');
-    alert.classList.add('alert-message');
-    alert.innerHTML = message;
+function showError(message) {
+  const alert = document.createElement("p");
+  alert.classList.add("alert-message");
+  alert.innerHTML = message;
 
-    form.appendChild(alert);
-    setTimeout(() => {
-        alert.remove();
-    }, 3000);
+  form.appendChild(alert);
+  setTimeout(() => {
+    alert.remove();
+  }, 3000);
 }
 
-function kelvinToCentigrade(temp){
-    return parseInt(temp - 273.15);
+function kelvinToCentigrade(temp) {
+  return parseInt(temp - 273.15);
 }
 
-function clearHTML(){
-    result.innerHTML = '';
+function clearHTML() {
+  result.innerHTML = "";
 }
